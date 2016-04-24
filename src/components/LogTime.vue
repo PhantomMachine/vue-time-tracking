74e446b7edd2a80ade8fbc20582447feb44157c9
doganozturk <doganozturk2005@gmail.com>
2016-04-24 03:37:48 +0300

    Building the app.

---
+++ src/components/LogTime.vue
@@ -0,0 +1,3 @@
+<template>
+  DENEME
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import Vue from 'vue';
 import App from './App';
+import Home from './components/Home';
+import TimeEntries from './components/TimeEntries';
+import LogTime from './components/LogTime';
+
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter();
+
+router.map({
+  '/home': {
+    component: Home,
+  },
+  '/time-entries': {
+    component: TimeEntries,
+  },
+  '/time-entries/log-time': {
+    component: LogTime,
+  },
+});
+
+router.redirect({
+  '*': '/home',
+});
+
+router.start(App, '#app');
 
 /* eslint-disable no-new */
 new Vue({
